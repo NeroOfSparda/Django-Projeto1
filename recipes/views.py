ac74e682e860118b9d9c7cf6caafa951c7e3c1ec
NeroOfSparda <andre_alfs12@hotmail.com>
2025-12-22 20:13:03 -0300

    Mudanças no settings e no views

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("HOME")
+    return render(request, 'recipes/home.html')
 
 def sobre(request):
     return HttpResponse("SOBRE")
